84f1312efae07833481bafe69b5ad6e8c4518694
JonathanValleA <jvalle@jaumebalmes.net>
2022-12-15 20:53:56 +0100

    Añadido exercicio 2 en el main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import exercici1 as ex1
 import exercici4_Joan_Gimenez as ex4
+import exercici2 as createArray
+import exercici2 as createArray2
+import exercici2 as createArray3
 #Joan
 #Exercici 1
 a = ex1.ex1()
@@ -14,3 +17,15 @@ print ("Número total d'elements (quants valors hi ha a la matriu)")
 print (a.size)
 print ("Tipus d'elements interns (si son integer, double,...)")
 print (a.dtype)
+
+#Jonathan
+#Exercici 2
+b = createArray.createArray()
+print("Array Funcion 1")
+print(b)
+c = createArray.createArray2()
+print("Array Funcion 2")
+print(c)
+d = createArray.createArray3()
+print("Array Funcion 3")
+print(d)
