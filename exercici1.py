a41d692d3d8debaf443ff7df5fa55bcb7ba0c562
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-12-15 19:10:05 +0100

    exercici1.py

---
+++ exercici1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def ex1():
+    #Fem una matriu de zeros de 50x50
+    a = np.zeros((50,50))
+    #Fem una matriu unidimensional que va de 0 a 49
+    arr = np.array(range(50), dtype='int32')
+    #Fiquem la matriu unidimensional a la diagonal de la matriu 50x50
+    a = np.diag(arr)
+    #Imprimim la matriu resultant
+    print (a)
+    #Guardem la matriu resultant a un arxiu npy
+    np.save('exercici1.npy', a)
